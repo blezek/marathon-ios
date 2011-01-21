bfbc2f5942e664628401653dc3612ea3bf553683
Daniel Blezek <daniel.blezek@gmail.com>
2011-01-20 21:41:45 -0600

    Support for stopping the game audio.

--- Classes/PreferencesViewController.h
+++ Classes/PreferencesViewController.h
@@ -11,25 +11,34 @@
 
 @interface PreferencesViewController : UIViewController {
   UISwitch *tapShoots;
+  UISwitch *crosshairs;
+  UISwitch *autoCenter;
   UISwitch *secondTapShoots;
   UISlider *sfxVolume;
   UISlider *musicVolume;
   UISlider *hSensitivity;
   UISlider *vSensitivity;
+  UISlider *brightness;
   UILabel *musicLabel;
+  UILabel *filmsDisabled;
 }
 
 @property (nonatomic, retain) IBOutlet UISwitch *tapShoots;
+@property (nonatomic, retain) IBOutlet UISwitch *autoCenter;
+@property (nonatomic, retain) IBOutlet UISwitch *crosshairs;
 @property (nonatomic, retain) IBOutlet UISwitch *secondTapShoots;
 @property (nonatomic, retain) IBOutlet UISlider *sfxVolume;
 @property (nonatomic, retain) IBOutlet UISlider *musicVolume;
 @property (nonatomic, retain) IBOutlet UILabel *musicLabel;
+@property (nonatomic, retain) IBOutlet UILabel *filmsDisabled;
 @property (nonatomic, retain) IBOutlet UISlider *hSensitivity;
 @property (nonatomic, retain) IBOutlet UISlider *vSensitivity;
+@property (nonatomic, retain) IBOutlet UISlider *brightness;
 
 + (void)setAlephOnePreferences:(BOOL)notifySoundManager;
 - (void)setupUI;
 - (IBAction)updatePreferences:(id)sender;
 - (IBAction)closePreferences:(id)sender;
+- (IBAction)notifyOfChanges;
 
 @end
--- Classes/PreferencesViewController.mm
+++ Classes/PreferencesViewController.mm
@@ -19,50 +19,83 @@
 @synthesize hSensitivity;
 @synthesize vSensitivity;
 @synthesize musicLabel;
+@synthesize crosshairs;
+@synthesize brightness;
+@synthesize autoCenter;
+@synthesize filmsDisabled;
 
 - (IBAction)closePreferences:(id)sender {
   
   // Save the back to defaults
   NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
   [defaults setBool:[self.tapShoots isOn] forKey:kTapShoots];
+  [defaults setBool:[self.crosshairs isOn] forKey:kCrosshairs];
   [defaults setBool:[self.secondTapShoots isOn] forKey:kSecondTapShoots];
   [defaults setFloat:self.sfxVolume.value forKey:kSfxVolume];
   [defaults setFloat:self.musicVolume.value forKey:kMusicVolume];
   [defaults setFloat:self.hSensitivity.value forKey:kHSensitivity];
-  // Note, only one sensitivity!
-  [defaults setFloat:self.hSensitivity.value forKey:kVSensitivity];
+  [defaults setFloat:self.brightness.value forKey:kGamma];
+  [defaults setFloat:self.hSensitivity.value forKey:kHSensitivity];
+  [defaults setFloat:self.vSensitivity.value forKey:kVSensitivity];
+  [defaults setBool:[self.autoCenter isOn] forKey:kAutocenter];
   [defaults synchronize];
   [PreferencesViewController setAlephOnePreferences:YES];
   [[AlephOneAppDelegate sharedAppDelegate].game closePreferences:sender];
+  Crosshairs_SetActive([defaults boolForKey:kCrosshairs]);
+
 }
 
 
 - (void)setupUI {
   NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
   self.tapShoots.on = [defaults boolForKey:kTapShoots];
+  self.crosshairs.on = [defaults boolForKey:kCrosshairs];
+  self.autoCenter.on = [defaults boolForKey:kAutocenter];
   self.secondTapShoots.on = [defaults boolForKey:kSecondTapShoots];
   self.sfxVolume.value = [defaults floatForKey:kSfxVolume];
   self.musicVolume.value = [defaults floatForKey:kMusicVolume];
   self.hSensitivity.value = [defaults floatForKey:kHSensitivity];
   self.vSensitivity.value = [defaults floatForKey:kVSensitivity];
+  self.brightness.value = [defaults floatForKey:kGamma];
 #if SCENARIO == 1
   self.musicLabel.hidden = NO;
   self.musicVolume.hidden = NO;
 #endif
+  [self notifyOfChanges];
 }
 
 - (IBAction)updatePreferences:(id)sender {
   [PreferencesViewController setAlephOnePreferences:YES];
 }
+- (IBAction)notifyOfChanges {
+  if ( self.autoCenter.on ) {
+    self.filmsDisabled.hidden = YES;
+  } else {
+    self.filmsDisabled.hidden = NO;
+  }
+}
+
+
 
 + (void)setAlephOnePreferences:(BOOL)notifySoundManager {
   MLog ( @"Set preferences from device back to engine" );
   NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
   sound_preferences->music = ceil ( (double)[defaults floatForKey:kMusicVolume] * (NUMBER_OF_SOUND_VOLUME_LEVELS-1) );
-  float sens = [defaults floatForKey:kHSensitivity];
+  float sens;
+  sens = [defaults floatForKey:kVSensitivity];
   if ( sens < 0.1 ) { sens = 0.1; }
   input_preferences->sens_vertical = _fixed ( sens * FIXED_ONE );
+  sens = [defaults floatForKey:kHSensitivity];
+  if ( sens < 0.1 ) { sens = 0.1; }
   input_preferences->sens_horizontal = _fixed ( sens * FIXED_ONE );
+  
+  // Auto center
+  if ( [defaults boolForKey:kAutocenter] ) {
+    input_preferences->modifiers &= ~_inputmod_dont_auto_recenter;
+  } else {
+    input_preferences->modifiers |= _inputmod_dont_auto_recenter;
+  }
+  
   SoundManager::instance()->SetParameters(*sound_preferences);
 
   MLog ( @"Music: %d", sound_preferences->music );
